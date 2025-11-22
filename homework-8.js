00bd75ec4bea569f9e74bdb4145ada7da3925061
MusaSagov <sagov82@mail.ru>
2025-11-22 15:01:36 +0300

    fixed

--- homework-8.js
+++ homework-8.js
@@ -4,24 +4,24 @@ import { productCards } from "./product-cards.js";
 const productCompound = document.querySelector(".product-card-list");
 const compositionTemplate = document.querySelector(".product-card-template");
 
-const createProductCard = productCard => {
-
-  const productCardClone = compositionTemplate.content.cloneNode(true);
-    productCardClone.querySelector(".images").src = `/images/${productCard.imageName}.png`;
-    productCardClone.querySelector(".product-category").textContent = productCard.category;
-    productCardClone.querySelector(".product-name").textContent = productCard.name;
-    productCardClone.querySelector(".product-price-label").innerHTML = `${productCard.price}&nbsp;₽`;
-    const ingredientsList = productCardClone.querySelector(".product-compound")
-    productCard.ingredients.forEach(ingredient => {
-    const li = document.createElement("li");    
-    li.textContent = ingredient;
-    ingredientsList.appendChild(li);
+const createProductCard = (productCardClone, productCard) => {
+  productCardClone.querySelector(".images").src = `/images/${productCard.imageName}.png`;
+  productCardClone.querySelector(".product-category").textContent = productCard.category;
+  productCardClone.querySelector(".product-name").textContent = productCard.name;
+  productCardClone.querySelector(".product-price-label").innerHTML = `${productCard.price}&nbsp;₽`;
+  const ingredientsList = productCardClone.querySelector(".product-compound")
+  productCard.ingredients.forEach(ingredient => {
+  const li = document.createElement("li");    
+  li.textContent = ingredient;
+  ingredientsList.appendChild(li);
   })
   productCompound.appendChild(productCardClone);
 };
 
-productCards.forEach(productCard => createProductCard(productCard));
-
+productCards.forEach(productCard => {
+  const productCardClone = compositionTemplate.content.cloneNode(true);
+  createProductCard(productCardClone, productCard);
+});
 
 
 //4. Использование метода .reduce(), для получения строки из название продуктовых карточек рзаделенной ";"
