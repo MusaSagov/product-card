fa3c59861e57b0302abf2681f3ffd0ee89dc6cb9
MusaSagov <sagov82@mail.ru>
2025-12-21 23:43:25 +0300

    fix

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -4,24 +4,25 @@ export class Modal {
     this.overlay = document.getElementById('overlay');
     this.shouldCloseOnOverlay = shouldCloseOnOverlay;
     this.#initOpen(buttonId);
-    this.#initCloseButton();
   }
 
   open() {
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
-    this.#addOverlayListener();
+    this.#addCloseListeners();
   }
 
   close() {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-    this.#removeOverlayListener();
+    this.#removeCloseListeners();
   }
 
   isOpen() {
     return this.modal.classList.contains('modal-showed');
   }
+
+  #handleClose = () => this.close();
 
   #initOpen(buttonId) {
     const button = document.getElementById(buttonId);
@@ -30,24 +31,23 @@ export class Modal {
     })
   }
 
-  #initCloseButton() {
+  #addCloseListeners() {
     const closeButton = this.modal.querySelector('#modal-close-button');
     if (closeButton) {
-      closeButton.addEventListener('click', () => this.close());
+      closeButton.addEventListener('click', this.#handleClose);
     }
-  }
-  
-  #overlayClickHandler = () => this.close();
-  
-  #addOverlayListener() {
     if (this.shouldCloseOnOverlay) {
-      this.overlay.addEventListener('click', this.#overlayClickHandler);
+      this.overlay.addEventListener('click', this.#handleClose);
     }
   }
 
-  #removeOverlayListener() {
+  #removeCloseListeners() {
+    const closeButton = this.modal.querySelector('#modal-close-button');
+    if (closeButton) {
+      closeButton.removeEventListener('click', this.#handleClose);
+    }
     if (this.shouldCloseOnOverlay) {
-      this.overlay.removeEventListener('click', this.#overlayClickHandler);
+      this.overlay.removeEventListener('click', this.#handleClose);
     }
   }
 }
